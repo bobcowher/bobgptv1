3f28bd5189fdad5b62f53b774829d3a9128c3f40
Robert Cowher <bobcowher@gmail.com>
2026-08-30 11:57:42 -0500

    We now have causal attention

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from importlib.metadata import version
 from dataset import *
 import tiktoken
 import torch
-from attention import SelfAttention_v1
+from attention import *
 
 inputs = torch.tensor(
         [[0.43, 0.15, 0.89],
@@ -14,39 +14,23 @@ inputs = torch.tensor(
         )
 
 query = inputs[1]
+d_in = inputs.shape[1]
+d_out = 2
 
-attention = SelfAttention_v1(d_in=6, d_out=6)
+# print(d_in)
+# print(d_out)
 
-# attn_scores = torch.empty(6, 6)
-#
-# for i, x_i in enumerate(inputs):
-#     for j, x_j in enumerate(inputs):
-#         attn_scores[i, j] = torch.dot(x_i, x_j) 
-#
-# print(attn_scores)
+batch = torch.stack((inputs, inputs), dim=0)
 
-# attn_scores = inputs @ inputs.T
-#
-# attn_weights = torch.softmax(attn_scores, dim=-1)
-#
-# print(attn_scores)
-# print(attn_weights)
-#
+context_length = batch.shape[1]
 
-# x_2   = inputs[1]
-# d_in  = inputs.shape[1]
-# d_out = 2
-#
-# torch.manual_seed(123)
-#
-# W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
-# W_key   = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
-# W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
-#
-# query_2 = x_2 @ W_query
-# key_2   = x_2 @ W_key
-# value_2 = x_2 @ W_value
-#
-# print(query_2)
-# print(key_2)
-# print(value_2)
+attention = CausalAttention(d_in=d_in, 
+                            d_out=d_out,
+                            context_length=context_length,
+                            dropout=0.1)
+
+
+context_vec = attention(batch)
+
+print(query)
+print(context_vec)
