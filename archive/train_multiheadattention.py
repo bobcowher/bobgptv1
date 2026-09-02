f24024eed560c76a8c06f19af92c6075e4ecdf7a
Robert Cowher <bobcowher@gmail.com>
2026-09-01 20:47:23 -0500

    Initial models script

---
+++ archive/train_multiheadattention.py
@@ -0,0 +1,43 @@
+from importlib.metadata import version
+from dataset import *
+import tiktoken
+import torch
+from attention import *
+
+inputs = torch.tensor(
+        [[0.43, 0.15, 0.89],
+         [0.55, 0.87, 0.66],
+         [0.57, 0.85, 0.64],
+         [0.22, 0.58, 0.33],
+         [0.77, 0.25, 0.10],
+         [0.05, 0.80, 0.55]]
+        )
+
+query = inputs[1]
+d_in = inputs.shape[1]
+d_out = 2
+
+# print(d_in)
+# print(d_out)
+
+batch = torch.stack((inputs, inputs), dim=0)
+
+context_length = batch.shape[1]
+
+# attention = CausalAttention(d_in=d_in, 
+#                             d_out=d_out,
+#                             context_length=context_length,
+#                             dropout=0.1)
+
+attention = MultiHeadAttention(d_in=d_in,
+                               d_out=d_out,
+                               context_length=context_length,
+                               dropout=0.1,
+                               num_heads=2)
+
+context_vec = attention(batch)
+
+# print(query)
+print(context_vec)
+print(context_vec.shape)
+
--- train.py
+++ train.py
@@ -3,41 +3,43 @@ from dataset import *
 import tiktoken
 import torch
 from attention import *
+from models import DummyGPTModel 
 
-inputs = torch.tensor(
-        [[0.43, 0.15, 0.89],
-         [0.55, 0.87, 0.66],
-         [0.57, 0.85, 0.64],
-         [0.22, 0.58, 0.33],
-         [0.77, 0.25, 0.10],
-         [0.05, 0.80, 0.55]]
-        )
+GPT_CONFIG_124M = {
+        "vocab_size": 50257,
+        "context_length": 1024,
+        "emb_dim": 768,
+        "n_heads": 12,
+        "n_layers": 12,
+        "drop_rate": 0.1,
+        "qkv_bias": False
+        }
 
-query = inputs[1]
-d_in = inputs.shape[1]
-d_out = 2
 
-# print(d_in)
-# print(d_out)
+tokenizer = tiktoken.get_encoding("gpt2")
+batch = []
 
-batch = torch.stack((inputs, inputs), dim=0)
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
 
-context_length = batch.shape[1]
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
 
-# attention = CausalAttention(d_in=d_in, 
-#                             d_out=d_out,
-#                             context_length=context_length,
-#                             dropout=0.1)
+torch.manual_seed(123)
 
-attention = MultiHeadAttention(d_in=d_in,
-                               d_out=d_out,
-                               context_length=context_length,
-                               dropout=0.1,
-                               num_heads=2)
+model = DummyGPTModel(cfg=GPT_CONFIG_124M)
 
-context_vec = attention(batch)
+logits = model(batch)
+print("Output shape:", logits.shape)
+# print(logits)
 
-# print(query)
-print(context_vec)
-print(context_vec.shape)
+single_embedding = logits[0][2]
+
+print(single_embedding.shape)
+print(single_embedding.mean())
+
+
+
+
 
